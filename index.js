36b1ffdee76e6a347ded8e3cf530d1316f537f5a
Minh Tran <tdaimike@gmail.com>
2023-03-30 14:34:43 -0700

    complete

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
     const newEmployee = Object.assign({}, employee, {[key]: value});
     return newEmployee;
 }
-
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value; 
+    employee[key] = value;
     return employee;
 }
+
 function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = Object.assign({},employee);
+    const newEmployee = Object.assign({}, employee);
     delete newEmployee[key];
     return newEmployee;
+
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
-
 }
